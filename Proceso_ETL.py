69eb7b6fdaa828119b8a7e4938dbc8da425ed16a
Luis <miguel_10_44@hotmail.com>
2024-08-08 16:32:41 -0500

    act

--- Proceso_ETL.py
+++ Proceso_ETL.py
@@ -153,3 +153,5 @@ if __name__== '__main__':
    atenciones=importarAtenciones(rutaAteciones)
    atenciones=transformacionAtenciones(atenciones)
    ConsolidadoTicketsAtenciones(tickets,atenciones,'sesion04')
+
+   
